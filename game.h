55769fdf77412d949bd0f511328262a6682ffac7
rfilipczak <rfilipczak3@gmail.com>
2021-02-02 21:34:55 +0100

    Introduced renderer initialization

--- game.h
+++ game.h
@@ -5,7 +5,10 @@
 #ifndef MINESWEEPER_GAME_H
 #define MINESWEEPER_GAME_H
 
-#define SIZE 8
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+#define BOARD_SIZE 8
 
 typedef enum {RUNNING} status_t;
 typedef enum {EMPTY} entity_t;
@@ -13,7 +16,7 @@ typedef enum {EMPTY} entity_t;
 
 typedef struct {
     status_t status;
-    int board[SIZE * SIZE];
+    int board[BOARD_SIZE * BOARD_SIZE];
 } game_t;
 
 #endif //MINESWEEPER_GAME_H
--- main.c
+++ main.c
@@ -11,15 +11,37 @@ int main(int argc, char *argv[]) {
     printf("Successfully initialized SDL.\n");
 
 
+    SDL_Window *window = NULL;
+    window = SDL_CreateWindow("Minesweeper Clone",
+                     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                     WINDOW_WIDTH, WINDOW_HEIGHT,
+                     0
+    );
+    if (window == NULL) {
+        fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    printf("Successfully created window.\n");
+
+    SDL_Renderer *renderer = NULL;
+    renderer = SDL_CreateRenderer(window, -1,
+                                  SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
+    if (renderer == NULL) {
+        fprintf(stderr, "Could not create renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    printf("Successfully created renderer.\n");
+
+
     game_t game = {
             .status = RUNNING,
             .board = {
                     EMPTY
             }
     };
-
-
-
 
 
 
