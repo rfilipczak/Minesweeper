67e1782a97997ce6bf5189e47b0aaa5b48e0ea7f
rfilipczak <rfilipczak3@gmail.com>
2021-02-03 13:06:00 +0100

    Fixed memory leak

--- rendering.c
+++ rendering.c
@@ -19,7 +19,7 @@ void render_contour(SDL_Renderer *renderer, const game_t *game)
     switch (game->difficulty) {
         case BEGINNER:
             empty_board_beginner = SDL_LoadBMP("empty_board_beginner.bmp");
-            empty_board_beginner_texture = SDL_CreateTextureFromSurface(renderer, empty_board_beginner); // WILL IT LEAK?
+            empty_board_beginner_texture = SDL_CreateTextureFromSurface(renderer, empty_board_beginner);
             SDL_Rect dstrect = { WINDOW_WIDTH / 2 - CONTOUR_BEGINNER_WIDTH / 2 - 5,
                                  WINDOW_HEIGHT / 2 - CONTOUR_BEGINNER_HEIGHT / 2, CONTOUR_BEGINNER_WIDTH, CONTOUR_BEGINNER_HEIGHT };
             SDL_RenderCopy(renderer, empty_board_beginner_texture, NULL, &dstrect);
@@ -28,7 +28,8 @@ void render_contour(SDL_Renderer *renderer, const game_t *game)
         default: {}
     }
 
-    SDL_DestroyTexture(empty_board_beginner_texture); // DOES IT FIX THE POSSIBLE LEAK?
+    SDL_DestroyTexture(empty_board_beginner_texture);
+    SDL_FreeSurface(empty_board_beginner);
 
 }
 
